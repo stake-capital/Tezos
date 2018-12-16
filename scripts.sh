692733f7ea039b856daed8c522a1dd74d1f07deb
Julien Bouteloup <julien.bouteloup@gmail.com>
2018-12-16 19:25:23 +0100

    Edit Tezos scripts doc

---
+++ scripts.sh
@@ -0,0 +1,15 @@
+# Docs: http://tezos.gitlab.io/mainnet/introduction/howtouse.html
+# http://tezos.gitlab.io/mainnet/introduction/howtorun.html
+
+## Basic Scripts
+### Run node with RPC interface
+./tezos-node run --rpc-addr 127.0.0.1
+
+### Get Balance
+./tezos-client get balance for stakeCapita
+
+### Receipt for a transaction
+./tezos-client rpc get /chains/main/blocks/head/operations
+
+### Review the receipt of the whole block
+./tezos-client rpc get /chains/main/blocks/head/metadata
